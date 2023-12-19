468f6e3748b6ee4b68eb2079257a61db475df997
adhaamtarekk <63559019+adhaamtarekk@users.noreply.github.com>
2023-12-19 05:27:33 +0200

    4th commit

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User.js");
 const validator = require("validator");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const dotenv = require("dotenv");
