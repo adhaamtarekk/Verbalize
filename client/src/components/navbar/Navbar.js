8aea630261aaab760fa909de841fa96708fb3d98
adhaamtarekk <63559019+adhaamtarekk@users.noreply.github.com>
2023-12-19 06:11:05 +0200

    client 2nd version commit

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ import "./Navbar.css";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5500/images/";
+  const PF = "https://verbalize.onrender.com/images/";
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
